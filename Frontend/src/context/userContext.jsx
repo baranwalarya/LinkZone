6d388290a27350f6715bb51f4604226eb4cfb729
Arya Baranwal <baranwalarya414@gmail.com>
2026-06-03 11:37:50 +0530

    updated project

--- Frontend/src/context/userContext.jsx
+++ Frontend/src/context/userContext.jsx
@@ -1,12 +1,24 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import { authDataContext } from './AuthContext.jsx'
+import axios from 'axios'
 export const userDataContext = createContext()
-function userContext({children}) {
+function UserContext({children}) {
 let [userData,setUserData] = useState([])
 let {serverUrl}=useContext(authDataContext)
 
-const getCurrentUser = 
+const getCurrentUser = async () => {
+  try {
+    let result=await axios.get(serverUrl+"api/user/currentuser",{withCredentials:true})
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
+
+useEffect(()=>{
+  getCurrentUser()
+},[])
 
   const value={}
 
@@ -19,4 +31,4 @@ const getCurrentUser =
   )
 }
 
-export default userContext
+export default UserContext
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import AuthContext from './context/AuthContext.jsx'
-import userContext from './context/userContext.jsx'
+import UserContext from './context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <AuthContext>
-    <userContext>
+    <UserContext>
       <App />
-    </userContext>
+    </UserContext>
   </AuthContext>
   </BrowserRouter>,
 )
