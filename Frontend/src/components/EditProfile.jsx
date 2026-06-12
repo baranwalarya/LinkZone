d52cf46ab6d53e3643cfd80763162a02421b61f5
Arya Baranwal <baranwalarya414@gmail.com>
2026-06-12 11:54:17 +0530

    updated project

--- Frontend/src/components/EditProfile.jsx
+++ Frontend/src/components/EditProfile.jsx
@@ -14,13 +14,48 @@ function EditProfile() {
     let [userName,setUserName] = useState(userData.userName || "")
     let [headline,setHeadline] = useState(userData.headline || "")
     let [location,setLocation] = useState(userData.location || "")
+    let [gender,setGender] = useState(userData.gender || "")
+    let [skills,setSkills] = useState(userData.skills || [])
+    let [newSkills,setNewSkills]=useState("")
+    let [education,setEducation] = useState(userData.education || [])
+    let [newEducation,setNewEducation]=useState({
+            college:"",
+            degree:"",
+            fieldOfStudy:"",
+        })
+
+    function addSkill(e){
+        e.preventDefault()
+        if(newSkills && !skills.includes(newSkills)){
+            setSkills([...skills,newSkills])
+        }
+        setNewSkills(" ")
+    }
+
+    function removeSkill(skill){
+        if(skills.includes(skill)){
+           setSkills(skills.filter((s)=>s!==skill))
+        }
+    }
+
+    function addEducation(e){
+        e.preventDefault()
+        if(newEducation.college && newEducation.degree && newEducation.fieldOfStudy){
+            setEducation([...education,newEducation])
+        }
+        setNewEducation({
+            college:"",
+            degree:"",
+            fieldOfStudy:"",
+        })
+    }
 
   return (
     <div className='w-full h-[100vh] fixed top-0 z-[200] flex justify-center items-center'>
         <div className='w-full h-full bg-black opacity-[0.5] absolute '>
         </div>
         <div className='w-[90%] max-w-[490px] h-[600px] bg-white relative z-[200] shadow-lg rounded-lg p-[10px] overflow-auto'>
-            <div className='absolute top-[20px] right-[20px] w-[25px] h-[25px] text-gray-800 font-semibold cursor-pointer ' onClick={()=>setEdit(false)}><RxCross1 className='w-[25px] h-[25px] text-gray-800 font-bold'/></div>
+            <div className='absolute top-[20px] right-[20px] w-[25px] h-[25px] text-gray-800 font-semibold cursor-pointer ' onClick={()=>setEdit(false)}><RxCross1 className='w-[25px] cursor-pointer h-[25px] text-gray-800 font-bold'/></div>
 
 
         <div className='w-full h-[150px] bg-gray-500 rounded-lg mt-[40px] overflow-hidden '>
@@ -36,19 +71,51 @@ function EditProfile() {
             <FiPlus className='text-[white]'/>
         </div>
 
-        <form className='w-full flex flex-col items-center justify-center gap-[20px] mt-[50px]'>
-            <input type="text" placeholder='firstName' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg'/>
-            <input type="text" placeholder='lastName' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg'/>
-            <input type="text" placeholder='userName' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg'/>
-            <input type="text" placeholder='headline' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg'/>
-            <input type="text" placeholder='location' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg'/>
-            <input type="text" placeholder='gender (male/female/other)' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg'/>
-            <div>
-                <h1>Skills</h1>
+        <div className='w-full flex flex-col items-center justify-center gap-[20px] mt-[50px]'>
+            <input type="text" placeholder='firstName' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg' value={firstName} onChange={(e)=>setFirstName(e.target.value)}/>
+            <input type="text" placeholder='lastName' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg' value={lastName} onChange={(e)=>setLastName(e.target.value)}/>
+            <input type="text" placeholder='userName' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg' value={userName} onChange={(e)=>setUserName(e.target.value)}/>
+            <input type="text" placeholder='headline' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg' value={headline} onChange={(e)=>setHeadline(e.target.value)}/>
+            <input type="text" placeholder='location' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg' value={location} onChange={(e)=>setLocation(e.target.value)}/>
+            <input type="text" placeholder='gender (male/female/other)' className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[18px] border-2 rounded-lg' value={gender} onChange={(e)=>setGender(e.target.value)}/>
 
+
+            {/* Skills */}
+            <div className='w-full p-[10px] border-2 border-gray-600 flex flex-col gap-[10px] rounded-lg'>
+                <h1 className='text-[19px] font-semibold'>Skills</h1>
+                {skills && <div className='flex flex-col gap-[10px]'>
+                    {skills.map((skill,index)=>(
+                        <div key={index} className='w-full h-[40px] border-[1px] border-gray-600 bg-gray-200 rounded-lg p-[10px] flex justify-between items-center'><span>{skill}</span> <RxCross1 className='w-[20px] h-[20px] text-gray-800 font-bold cursor-pointer' onClick={()=>removeSkill(skill)}/> </div>
+                    ))}
+                </div>}
+                <div className='flex flex-col gap-[10px] items-start' >
+                    <input type="text" placeholder='add new skill' value={newSkills} onChange={(e)=>setNewSkills(e.target.value)} className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[16px] border-2 rounded-lg'/>
+                    <button className='w-[100%] h-[40px] rounded-full border-2 border-[#2dc0ff] text-[#2dc0ff]' onClick={addSkill}>Add</button>
+                </div>
             </div>
 
-        </form>
+            {/* Education */}
+             <div className='w-full p-[10px] border-2 border-gray-600 flex flex-col gap-[10px] rounded-lg'>
+                <h1 className='text-[19px] font-semibold'>Education</h1>
+                {education && <div className='flex flex-col gap-[10px]'>
+                    {education.map((edu,index)=>(
+                        <div key={index} className='w-full h-[40px] border-[1px] border-gray-600 bg-gray-200 rounded-lg p-[10px] flex justify-between items-center'><div>
+                                <div>College:{edu.college}</div>
+                                <div>Degree:{edu.degree}</div>
+                                <div>Field Of Study:{edu.fieldOfStudy}</div>
+                            </div> 
+                            <RxCross1 className='w-[20px] h-[20px] text-gray-800 font-bold cursor-pointer' /> </div>
+                    ))}
+                </div>}
+                <div className='flex flex-col gap-[10px] items-start' >
+                    <input type="text" placeholder='college' value={newEducation.college} onChange={(e)=>setNewEducation({...newEducation,college:e.target.value})} className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[16px] border-2 rounded-lg'/>
+                    <input type="text" placeholder='degree' value={newEducation.degree} onChange={(e)=>setNewEducation({...newEducation,degree:e.target.value})} className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[16px] border-2 rounded-lg'/>
+                    <input type="text" placeholder='fieldOfStudy' value={newEducation.fieldOfStudy} onChange={(e)=>setNewEducation({...newEducation,fieldOfStudy:e.target.value})} className='w-full h-[50px] outline-none border-gray-600 px-[10px] py-[2px] text-[16px] border-2 rounded-lg'/>
+                    <button className='w-[100%] h-[40px] rounded-full border-2 border-[#2dc0ff] text-[#2dc0ff]' onClick={addEducation}>Add</button>
+                </div>
+            </div>
+                
+        </div>
 
         </div>
     </div>
