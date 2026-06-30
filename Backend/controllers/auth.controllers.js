874c01e0eca4a389bc75fb0fdbfdb5729a0ca323
Arya Baranwal <baranwalarya414@gmail.com>
2026-06-30 15:37:04 +0530

    updated project

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -32,8 +32,8 @@ export const signUp=async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:process.env.NODE_ENVIRONMENT==="production"
+            sameSite:"none",
+            secure:true
         })
         return res.status(201).json(user)
 
@@ -61,8 +61,8 @@ export const login = async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             maxAge:7*24*60*60*1000,
-            sameSite:"strict",
-            secure:process.env.NODE_ENVIRONMENT==="production"
+            sameSite:"none",
+            secure:true
         })
         return res.status(200).json(user)
     } catch (error) {
@@ -73,8 +73,12 @@ export const login = async (req,res) => {
 
 export const logOut = async (req,res) => {
     try {
-        res.clearCookie("token")
-        return res.status(200).json({message:"ILogout successfully!"})
+        res.clearCookie("token",{
+            httpOnly:true,
+            sameSite:"none",
+            secure:true
+        })
+        return res.status(200).json({message:"Logout successfully!"})
     } catch (error) {
         console.log(error)
         return res.status(500).json({message:"logout error"})
